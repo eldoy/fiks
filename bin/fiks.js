5125d0f6d77797239642365e7481b2a53f6a0510
Vidar Eldøy <vidar@eldoy.com>
2024-08-05 02:01:27 +0200

    Simplification

--- bin/fiks.js
+++ bin/fiks.js
@@ -1,5 +1,66 @@
 #!/usr/bin/env node
+var extras = require('extras')
+var terminal = require('xecutor')
+var farge = require('farge')()
+var commands = {
+  link: require('../lib/link.js'),
+  sync: require('../lib/sync.js'),
+  status: require('../lib/status.js'),
+  help: require('../lib/help.js')
+}
 
-var main = require('../lib/main.js')
+var SYMBOLS = { ok: '✔', no: '✖' }
+var COMMANDS = Object.keys(commands)
 
+global.J = function (obj) {
+  console.log(JSON.stringify(obj, null, 2))
+}
+
+function pad(text) {
+  return text
+    .split('\n')
+    .map((line) => `  ${line}`)
+    .join('\n')
+}
+
+async function main() {
+  var root = process.cwd()
+
+  try {
+    var workspace = extras.read('./workspace.yml')
+  } catch {
+    console.info('No workspace.yml file found.')
+    process.exit()
+  }
+  // console.log(J(workspace))
+
+  var repos = workspace.repos || []
+  if (!repos.length) {
+    console.info('Repos are empty. Add them to you workspace file.')
+    process.exit()
+  }
+
+  var struct = require('../lib/struct.js').load(root, workspace.repos)
+  // console.log(J(struct))
+
+  var { $ } = new terminal()
+
+  var command = process.argv[2]
+
+  if (!command) {
+    console.info(`Command missing. Run "fiks help" for usage.`)
+    process.exit()
+  }
+
+  if (!COMMANDS.includes(command)) {
+    console.info(`Invalid command: status\n`)
+    console.info(`Must be one of:`)
+    console.info(COMMANDS.map((x) => `  ${x}`).join('\n'))
+    process.exit()
+  }
+
+  await commands[command]({ $, repos, struct })
+
+  process.exit()
+}
 main()
---
+++ docs/COMMANDS.md
@@ -0,0 +1,12 @@
+### Status
+
+Shows status for each repo, green for nothing to do, red for unstaged changes.
+
+For each directory we run:
+
+- git remote update
+- git status
+
+https://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git
+
+fiks status -r admin search
